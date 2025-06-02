c6d54f71a28137a4cdb07ff5f9e0e1d014494bfd
Sri Harsha Gangavelli <harshagangavelli17@gmail.com>
2025-06-02 21:03:57 +0530

    Added servlet initializer

--- src/main/java/com/tcs/students/StudentsApplication.java
+++ src/main/java/com/tcs/students/StudentsApplication.java
@@ -2,12 +2,20 @@ package com.tcs.students;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StudentsApplication {
+public class StudentsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentsApplication.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StudentsApplication.class);
     }
 
 }
