88e54d139350ea33f20bd8defdc2538ff35cc239
Sri Harsha Gangavelli <harshagangavelli17@gmail.com>
2025-06-03 11:52:46 +0530

    Added context path

---
+++ src/main/resources/static/js/studentmanagement.js
@@ -0,0 +1,4 @@
+  const currentUrl = new URL(window.location.href);
+  const currentPort = currentUrl.port ? `:${currentUrl.port}` : '';
+  const currentContextPath = 'student-management';
+  const BASE_URL = `${currentUrl.protocol}//${currentUrl.hostname}${currentPort}/${currentContextPath}/`;
