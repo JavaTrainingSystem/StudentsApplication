3fce0505fe1e0fe29a98d618a4fe14dfa4b22ab7
Sri Harsha Gangavelli <harshagangavelli17@gmail.com>
2025-06-03 14:58:08 +0530

    changes in html and js

--- src/main/resources/static/js/studentmanagement.js
+++ src/main/resources/static/js/studentmanagement.js
@@ -2,3 +2,22 @@
   const currentPort = currentUrl.port ? `:${currentUrl.port}` : '';
   const currentContextPath = '';
   const BASE_URL = `${currentUrl.protocol}//${currentUrl.hostname}${currentPort}${currentContextPath ? '/' + currentContextPath + '/' : '/' + 'api/v1/'}`;
+
+  const tokenVar = "accessToken";
+
+  function setTokenDetails(token) {
+    window.localStorage.setItem(tokenVar, 'Bearer ' + token);
+  }
+
+  function getToken() {
+    return window.localStorage.getItem(tokenVar);
+  }
+
+  function removeToken() {
+    window.localStorage.removeItem(tokenVar);
+  }
+
+  function handleUnauthorized() {
+    removeToken();
+    window.location.href = "login";
+  }
