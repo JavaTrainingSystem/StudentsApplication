fd9f7aebba067817fe0642a14be244fdce47d99a
Sri Harsha Gangavelli <harshagangavelli17@gmail.com>
2025-06-16 20:17:21 +0530

    added userid method

--- src/main/java/com/tcs/students/utils/CommonUtils.java
+++ src/main/java/com/tcs/students/utils/CommonUtils.java
@@ -1,16 +1,22 @@
 package com.tcs.students.utils;
 
+import com.tcs.students.dao.UserRepo;
+import com.tcs.students.entity.UserEntity;
 import com.tcs.students.exceptions.handlers.UnAuthorizedException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
+@RequiredArgsConstructor
 public class CommonUtils {
+
+    private final UserRepo userRepo;
 
     public String getCurrentLoggedInUserName() {
         String authorization = getCurrentRequest().getHeader("Authorization");
@@ -28,6 +34,11 @@ public class CommonUtils {
 
     public HttpServletRequest getCurrentRequest() {
         return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    }
+
+    public Integer getCurrentLoggedInUserId() {
+        UserEntity userEntity = userRepo.findByUserName(getCurrentLoggedInUserName());
+        return userEntity.getUserId();
     }
 
     public void isAdmin() {
